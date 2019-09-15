ee6ab6b6fb12f3a5b84b2152477eede4906ddf31
clarymt <clarymt@me.com>
2019-09-15 13:59:49 -0400

    got gifs to appear by changing one letter LOL

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
 $(document).ready(function() {
 
-var horrors = ["evil dead", "evil dead 2", "army of darkness", "ghost", "demon", "necronomicon", "haunting", "the exorcist", "hellraiser", "satan"]
+var horrors = ["evil dead", "evil dead 2", "army of darkness", "ghost", "demon", "necronomicon", "haunting", "the exorcist", "hellraiser", "satan", "your pretty face is going to hell", "are you afraid of the dark", "beetlejuice", "saw", "the toxic avenger", "killer clowns from outer space"]
 
 function renderButtons() {
 	$("#horror-buttons").empty();
 	for (i = 0; i < horrors.length; i++) {
-		$("#horror-buttons").append("<button class='btn btn-danger' data-movie='" + horrors[i] + "'>" + horrors[i] + "</button>");
+		$("#horror-buttons").append("<button class='btn btn-danger' data-horror='" + horrors[i] + "'>" + horrors[i] + "</button>");
 		}
     }
     renderButtons();
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        horror + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-
+    $("button").on("click", function() {
+        // Grabbing and storing the data-animal property value from the button
+        var horrors = $(this).attr("data-horror");
+  
+        // Constructing a queryURL using the animal name
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          horrors + "&api_key=aNKEB7vfbBsZhY4hJXxNB21jU16vOjYk&limit=10";
+  
+        // Performing an AJAX request with the queryURL
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          // After data comes back from the request
+          .then(function(response) {
+            console.log(queryURL);
+  
+            console.log(response);
+            // storing the data from the AJAX request in the results variable
+            var results = response.data;
+  
+            // Looping through each result item
+            for (var i = 0; i < results.length; i++) {
+  
+              // Creating and storing a div tag
+              var horrorDiv = $("<div>");
+  
+              // Creating a paragraph tag with the result item's rating
+              var p = $("<p>").text("Rating: " + results[i].rating);
+  
+              // Creating and storing an image tag
+              var horrorImage = $("<img>");
+              // Setting the src attribute of the image to a property pulled off the result item
+              horrorImage.attr("src", results[i].images.fixed_height.url);
+  
+              // Appending the paragraph and image tag to the animalDiv
+              horrorDiv.append(p);
+              horrorDiv.append(horrorImage);
+  
+              // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+              $("#horrors").prepend(horrorDiv);
+            }
+          });
+      });
 });
